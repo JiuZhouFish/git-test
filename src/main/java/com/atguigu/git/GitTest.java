4a585e223ca0f877faa52c2e9d68cadac903690a
zhangxy <1583693360@qq.com>
2022-08-07 21:53:15 +0800

    master commit3

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,5 +13,7 @@ public class GitTest {
         System.out.println("Test Git5");
         System.out.println("master Test");
         System.out.println("hot-fix test!");
+        System.out.println("pull test!");
+        
     }
 }
