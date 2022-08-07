1527f884b804afd1637fb9939074002d321d34a4
zhangxy <1583693360@qq.com>
2022-08-07 17:52:09 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,6 +8,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Test Git");
         System.out.println("Test Git2");
-        
+        System.out.println("Test Git3");
+        System.out.println("Test Git4");
+        System.out.println("Test Git5");
     }
 }
