3fa498d08940eb62793061f96328adf547016d1b
zhangxy <1583693360@qq.com>
2022-08-07 17:28:18 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author zxy
+ * @date 2022/8/7 - 17:25
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Test Git");
+        
+    }
+}
